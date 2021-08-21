dbb1f46c0250f121d3738ec755ffc9567345d4d7
katenjoki <katenjoki98@gmail.com>
2021-08-21 18:29:44 +0000

    Final update

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -5,9 +5,9 @@ DATA_PATH = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/" #lidar path
 REGION = "IA_FullState/"
 BOUNDS = "([-10425171.940, -10423171.940], [5164494.710, 5166494.710])"
 ACCESS_PATH = DATA_PATH + REGION + "ept.json"
-OUTPUT_FILENAME_LAZ = "laz/iowa.laz"
-OUTPUT_FILENAME_TIF = "tif/iowa.tif"
-PIPELINE_PATH = "get_data.json"
+OUTPUT_FILENAME_LAZ = "../laz/iowa.laz"
+OUTPUT_FILENAME_TIF = "../tif/iowa.tif"
+PIPELINE_PATH = "../data/get_data.json"
 
 def get_raster_terrain(bounds:str, region:str, access_path:str = ACCESS_PATH, output_filename_laz:str = OUTPUT_FILENAME_LAZ,
                       output_filename_tif:str = OUTPUT_FILENAME_TIF, pipeline_path:str = PIPELINE_PATH)->None:
